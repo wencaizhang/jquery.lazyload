0a9c5ff15ea5c0852c35cb8133b5c6abc12c5bfe
wencaizhang <1052642137@qq.com>
2019-05-24 16:11:12 +0800

    update docs

---
+++ docs/jquery.lazyload.js
@@ -0,0 +1,47 @@
+(function($, win, doc, undefined) {
+  var pluginName = "lazyload";
+
+  function Plugin(ele, options) {
+    var defaultOptions = {
+      distance: 10,
+      immediate: false,
+    };
+    this.ele = $(ele);
+    this.options = $.extend({}, defaultOptions, options);
+    this.init();
+  }
+  Plugin.prototype = {
+    init: function() {
+      var self = this;
+      var ele = self.ele;
+      self.src = ele.attr("data-src");
+      self.offsetTop = self.ele.offset().top;
+      if (self.options.immediate) {
+        self.watch();
+      }
+      $(doc).on("scroll", function() {
+        self.watch();
+      });
+      $(win).on("resize", function() {
+        self.watch();
+      });
+    },
+    watch: function() {
+      var self = this;
+      var scrollTop = $(doc).scrollTop();
+      var clientHeight = document.documentElement.clientHeight;
+      var distance = this.offsetTop - scrollTop - clientHeight;
+      console.log(distance)
+      if (distance < self.options.distance) {
+        self.ele.attr("src", self.src);
+      }
+    }
+  };
+
+  $.fn[pluginName] = function(options) {
+    this.each(function() {
+      new Plugin(this, options);
+    });
+    return this;
+  };
+})(jQuery, window, document);
